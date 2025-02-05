25c898ada965cf1fcb3cbe5945a1bf53712bc96c
Andrey Isenko <aisenko87@gmail.com>
2025-02-05 12:36:29 +0200

    add repo-hw-03

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+import "./App.css"
+
+function App() {
+
+  return (
+  
+      <div>
+       <p>goit-react-hw-03</p>
+      </div>
+   
+  );
+}
+
+export default App;
